0a56cda0cd9a4e7fd6f08cf76cfae317e38cb68a
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2025-11-21 17:54:57 -0500

    Init api

--- packages/auth/src/routes/auth.routes.ts
+++ packages/auth/src/routes/auth.routes.ts
@@ -4,6 +4,10 @@ import { requireAuth } from "../middleware/requireAuth";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.json({ message: "VidSync API" });
+});
+
 router.post("/register", AuthController.register);
 router.post("/verify-token", AuthController.verifyToken);
 
