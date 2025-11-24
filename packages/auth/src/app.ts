9fc986a41477a7620ecc7c59bb9d840e2b0678b5
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2025-11-24 12:54:53 -0500

    Handle cors paths

--- packages/auth/src/app.ts
+++ packages/auth/src/app.ts
@@ -1,11 +1,29 @@
 import express from "express";
 import routes from "./routes/auth.routes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
+const whitelist = ['http://localhost:5173', 'https://vid-sync-frontend-nu.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+    if (origin && whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      console.log(origin)
+      console.error('Not allowed by CORS')
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
+
 const app = express();
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use("/api/auth", routes);
 
